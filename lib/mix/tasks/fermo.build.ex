408209ae0d5b5f509a920bf5565bbfb49629a916
Joe Yates <joe.g.yates@gmail.com>
2017-05-18 21:54:20 +0200

    Run the current project's 'config' function

--- lib/mix/tasks/fermo.build.ex
+++ lib/mix/tasks/fermo.build.ex
@@ -6,8 +6,14 @@ defmodule Mix.Tasks.Fermo.Build do
   @moduledoc """
   Builds the project according to the settings in priv/config.exs
   """
-  def run(args) do
-    {:ok, output} = Fermo.render("article_template.html.slim")
-    IO.puts "output: #{output}"
+  def run(_args) do
+    Mix.Task.run "app.start"
+    project = Mix.Project.get()
+    [main | _rest] = Module.split(project)
+    module = String.to_existing_atom("Elixir.#{main}")
+    config = module.config()
+    config = Fermo.build(config)
+    IO.puts "config: #{inspect(config, [pretty: true, width: 0])}"
+    {:ok}
   end
 end
