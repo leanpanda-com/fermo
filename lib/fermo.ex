5e502581e20b0e1f9eb644524b2371aed72c5621
Joe Yates <joe.g.yates@gmail.com>
2017-06-25 21:26:55 +0200

    Build basic pages

--- lib/fermo.ex
+++ lib/fermo.ex
@@ -2,10 +2,62 @@ defmodule Fermo do
   require EEx
   require Slime
 
-  def build(config \\ %{}) do
-    # TODO: build stuff that's not excluded
-    # TODO: run external pipelines
-    config
+  @doc false
+  defmacro __using__(opts \\ %{}) do
+    quote do
+      require Fermo
+
+      @before_compile Fermo
+      Module.register_attribute __MODULE__, :config, persist: true
+      @config unquote(opts)
+
+      def link_to(text, href, attributes \\ []) do
+        attribs = Enum.map(attributes, fn ({k, v}) ->
+          "#{k}=\"#{v}\""
+        end)
+        "<a href=\"#{href}\" #{Enum.join(attribs, " ")}>#{text}</a>"
+      end
+    end
+  end
+
+  @doc false
+  defmacro __before_compile__(env) do
+    templates = File.cd!("priv/source", fn ->
+      Path.wildcard("**/*.slim")
+    end)
+    defs = Enum.map(templates, fn (path) ->
+      Fermo.deftemplate(path)
+    end)
+
+    config = Module.get_attribute(env.module, :config)
+    exclude = Map.get(config, :exclude, [])
+    exclude_matchers = Enum.map(exclude, fn (glob) ->
+      single = String.replace(glob, "?", ".")
+      multiple = String.replace(single, "*", ".*")
+      Regex.compile!(multiple)
+    end)
+
+    pages = Enum.reduce(templates, [], fn (path, acc) ->
+      skip = Enum.any?(exclude_matchers, fn (exclude) ->
+        Regex.match?(exclude, path)
+      end)
+      if skip do
+        acc
+      else
+        [path | acc]
+      end
+    end)
+
+    config = Map.put(config, :pages, pages)
+    Module.put_attribute(env.module, :config, config)
+    IO.puts "__before_compile__ @config: #{inspect(config, [pretty: true, width: 0])}"
+
+    get_config = quote do
+      def config() do
+        hd(__MODULE__.__info__(:attributes)[:config])
+      end
+    end
+    defs ++ [get_config]
   end
 
   defp full_template_path(path) do
@@ -13,45 +65,80 @@ defmodule Fermo do
   end
 
   defp source_path, do: "priv/source"
+  defp build_path, do: "build"
 
-  def proxy(conf, template, target, arguments \\ []) do
-    [eex, frontmatter] = prepare_proxy(template)
-    bindings = combine(frontmatter, arguments)
-    output = EEx.eval_string(eex, bindings)
+  def deftemplate(template) do
+    [frontmatter, body] = parse_template(template)
+    eex_source = Slime.Renderer.precompile(body)
+    name = String.to_atom(template)
+    quote bind_quoted: binding() do
+      info = [file: __ENV__.file, line: __ENV__.line]
+      compiled = EEx.compile_string(eex_source, info)
+      escaped_frontmatter = Macro.escape(frontmatter)
+      args = [Macro.var(:params, nil)]
+
+      # Define a method with the frontmatter, so we can merge with
+      # params when the template is evaluated
+      def unquote(:"#{name}-defaults")() do
+        unquote(escaped_frontmatter)
+      end
+
+      def unquote(name)(unquote_splicing(args)) do
+        unquote(compiled)
+      end
+    end
+  end
+
+  defmacro proxy(conf, template, target, params \\ %{}) do
+    quote bind_quoted: binding() do
+      Fermo.build_page(__MODULE__, conf, template, target, params)
+    end
+  end
+
+  def build_page(module, conf, template, target, params \\ %{}) do
+    defaults_method = String.to_atom(template <> "-defaults")
+    defaults = apply(module, defaults_method, [])
+    args = Map.merge(defaults, params)
+    name = String.to_atom(template)
+    output = apply(module, name, [args])
 
     resource = %{body: output, target: target}
     resources = Map.get(conf, :resources, [])
     put_in(conf, [:resources], resources ++ [resource])
   end
 
-  defp prepare_proxy(template) do
-    [frontmatter, body] = split_template(template)
-    eex = Slime.Renderer.precompile(body)
-    [eex, frontmatter]
+  defmacro build(config \\ %{}) do
+    quote bind_quoted: binding() do
+      Fermo.do_build(__MODULE__, config)
+    end
   end
 
-  defp split_template(path) do
-    {:ok, source} = File.read(full_template_path(path))
-    [frontmatter_yaml, body] = String.split(source, "---\n")
-    frontmatter = YamlElixir.read_from_string(frontmatter_yaml)
-    [frontmatter, body]
-  end
-
-  defp combine(list1, list2) do
-    Keyword.merge(keyword_list(list1), keyword_list(list2), fn _k, _v1, v2 ->
-      v2
+  def do_build(module, config) do
+    pages = config[:pages]
+    config = Enum.reduce(pages, config, fn (page, config) ->
+      "localizable/" <> path  = page
+      path = String.replace(path, ".slim", "")
+      build_page(module, config, page, path, %{})
     end)
+
+    resources = config[:resources]
+    File.mkdir(build_path())
+    Enum.each(resources, fn (%{body: body, target: target}) ->
+      pathname = Path.join(build_path(), target)
+      path = Path.dirname(pathname)
+      File.mkdir(path)
+      File.write!(pathname, body, [:write])
+    end)
+    config
   end
 
-  def keyword_pair({key, value}) when is_atom(key) do
-    {key, value}
-  end
-  def keyword_pair({key, value}) do
-    {String.to_atom(key), value}
+  def parse_template(path) do
+    File.read(full_template_path(path)) |> split_template
   end
 
-  defp keyword_list(map) when is_map(map) do
-    Enum.map(map, &__MODULE__.keyword_pair/1)
+  def split_template({:ok, source = "---\n" <> _rest}) do
+    [_, frontmatter_yaml, body] = String.split(source, "---\n")
+    frontmatter = YamlElixir.read_from_string(frontmatter_yaml)
+    [Macro.escape(frontmatter, unquote: true), body]
   end
-  defp keyword_list(list), do: list
 end
