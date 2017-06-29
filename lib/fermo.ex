db14289d4bb948ec7f22f0cc5a1ee4696d739fda
Joe Yates <joe.g.yates@gmail.com>
2017-06-29 20:25:17 +0200

    We need to compile inside the quote

--- lib/fermo.ex
+++ lib/fermo.ex
@@ -127,24 +127,24 @@ defmodule Fermo do
   def deftemplate(template) do
     [frontmatter, body] = parse_template(template)
     body = String.replace(body, ~r/^[\s\r\n]*/, "")
-    compiled =
-      try do
-        eex_source = Slime.Renderer.precompile(body)
-        info = [file: template, line: 1]
-        EEx.compile_string(eex_source, info)
-      rescue
-        e ->
-          IO.puts "Failed to precompile template: '#{template}'"
-          IO.puts "\nbody:\n#{body}\n"
-          raise e
-      catch
-        e ->
-          IO.puts "Failed to precompile template: '#{template}'"
-          IO.puts "\nbody:\n#{body}\n"
-          raise e
-      end
     name = String.to_atom(template)
     quote bind_quoted: binding() do
+      compiled =
+        try do
+          eex_source = Slime.Renderer.precompile(body)
+          info = [file: template, line: 1]
+          EEx.compile_string(eex_source, info)
+        rescue
+          e ->
+            IO.puts "Failed to precompile template: '#{template}'"
+          IO.puts "\nbody:\n#{body}\n"
+          raise e
+        catch
+          e ->
+            IO.puts "Failed to precompile template: '#{template}'"
+          IO.puts "\nbody:\n#{body}\n"
+          raise e
+        end
       escaped_frontmatter = Macro.escape(frontmatter)
       args = [Macro.var(:params, nil)]
 
