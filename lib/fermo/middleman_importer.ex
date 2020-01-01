20c30d83335961eebe993a8df694ad2f1d2e569a
Joe Yates <joe.g.yates@gmail.com>
2020-01-01 11:10:46 +0100

    Add more replacements

--- lib/fermo/middleman_importer.ex
+++ lib/fermo/middleman_importer.ex
@@ -53,10 +53,17 @@ defmodule Fermo.MiddlemanImporter do
     raw
     |> ifs
     |> eachs
+    |> partial_params
     |> ts
+    |> counts
+    |> presents
   end
 
   @if_match ~r<(\s*)-\s*if(.*)>x
+
+  defp ifs(raw) do
+    String.replace(raw, @if_match, "\\1= if\\2 do")
+  end
 
   # Match `  - foos.each do |foo|`
   @each_match ~r<
@@ -68,26 +75,61 @@ defmodule Fermo.MiddlemanImporter do
     \|([\w_]+)\|        # block parameters
   >x
 
+  defp eachs(raw) do
+    String.replace(raw, @each_match, "\\1= Enum.map \\2, fn \\3 ->")
+  end
+
+  # Match `= partial "foo", locals: {bar: "baz"}`
+  @partial_params_match ~r<
+    (\s*)
+    =\s*
+    partial\s+
+    ['"]
+    ([\w\/\-\ "']+)
+    ['"]
+    ,\s*
+    locals:\s*
+    {
+    ([^}]+)
+    }
+  >x
+
+  defp partial_params(raw) do
+    String.replace(raw, @partial_params_match, ~s<\\1= partial "\\2", %{\\3}>)
+  end
+
   # Match `t("teh.label")`
   @translation_match ~r<
     \bt                 # t
     \(                  # (
     ['"]                # ' or "
-    ([\w_\.]+)          # localized string identifier
+    ([\w_\.\-]+)        # localized string identifier
     ['"]                # ' or "
     \)                  # )
   >x
 
-  defp ifs(raw) do
-    String.replace(raw, @if_match, "\\1= if\\2 do")
-  end
-
-  defp eachs(raw) do
-    String.replace(raw, @each_match, "\\1= Enum.map \\2, fn \\3 ->")
-  end
-
   defp ts(raw) do
     String.replace(raw, @translation_match, ~s<t("\\1", locale)>)
+  end
+
+  # Match `foos.count`
+  @count_match ~r<
+    \b([\w_\.]+)        # a Ruby variable
+    \.count             # `.count`
+  >x
+
+  defp counts(raw) do
+    String.replace(raw, @count_match, ~s<length(\\1)>)
+  end
+
+  # Match `foo.present?`
+  @present_match ~r<
+    \b([\w_\.]+)        # a Ruby variable
+    \.present\?         # `.present?`
+  >x
+
+  defp presents(raw) do
+    String.replace(raw, @present_match, ~s<\\1>)
   end
 
   defp ensure_directory(path) do
