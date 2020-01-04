aaaca4085b82881622aea300dac540c21d7a93cb
Joe Yates <joe.g.yates@gmail.com>
2020-01-04 14:12:53 +0100

    Version bump for release

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Fermo.MixProject do
   def project do
     [
       app: :fermo,
-      version: "0.2.1",
+      version: "0.2.2",
       elixir: "~> 1.4",
       description: "A static site generator",
       package: package(),
