be8a54d34a0bcdb7f85bc0a7832c162087c0f094
Joe Yates <joe.g.yates@gmail.com>
2020-01-05 11:45:33 +0100

    Fixup: Elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,8 @@ defmodule Fermo.MixProject do
     [
       app: :fermo,
       version: "0.1.0",
-      elixir: "~> 1.9",
+      elixir: "~> 1.4",
+      build_embedded: Mix.env == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
