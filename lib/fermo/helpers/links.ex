f0cb49d9689070889431eaa1454177dae23d8b56
Joe Yates <joe.g.yates@gmail.com>
2020-01-05 15:02:49 +0100

    Improve link_to

--- lib/fermo/helpers/links.ex
+++ lib/fermo/helpers/links.ex
@@ -4,17 +4,21 @@ defmodule Fermo.Helpers.Links do
     quote do
       require Fermo.Helpers.Links
 
-      def link_to(href, attributes, [do: content] = other) when is_list(attributes) and is_list(other) do
+      def link_to(href, [do: content] = other) do
+        link_to(content, href, [])
+      end
+      def link_to(text, href) when is_binary(text) and is_binary(href) do
+        "<a href=\"#{href}\">#{text}</a>"
+      end
+
+      def link_to(href, attributes, [do: content] = other) when is_binary(href) and is_list(attributes) do
         link_to(content, href, attributes)
       end
-      def link_to(text, href, attributes) do
+      def link_to(text, href, attributes) when is_binary(text) and is_binary(href) and is_list(attributes) do
         attribs = Enum.map(attributes, fn ({k, v}) ->
           "#{k}=\"#{v}\""
         end)
         "<a href=\"#{href}\" #{Enum.join(attribs, " ")}>#{text}</a>"
-      end
-      def link_to(text, href) do
-        link_to(text, href, [])
       end
 
       def mail_to(email, caption \\ nil, _mail_options \\ %{}) do
