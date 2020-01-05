8023585baf28d62f57fdbe1435b5498b28d61d21
Joe Yates <joe.g.yates@gmail.com>
2020-01-05 11:49:25 +0100

    Prepare for release

--- mix.exs
+++ mix.exs
@@ -6,6 +6,8 @@ defmodule Fermo.MixProject do
       app: :fermo,
       version: "0.1.0",
       elixir: "~> 1.4",
+      description: "A static site generator",
+      package: package(),
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps()
@@ -21,10 +23,21 @@ defmodule Fermo.MixProject do
 
   defp deps do
     [
+      {:ex_doc, "~> 0.15.1", only: :dev},
       {:exjsx, "~> 3.2"},
       {:morphix, "~> 0.0.7"},
       {:slime, "~> 1.0.0"},
       {:yaml_elixir, "~> 1.3.0"}
     ]
+  end
+
+  defp package do
+    %{
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/joeyates/fermo"
+      },
+      maintainers: ["Joe Yates"]
+    }
   end
 end
