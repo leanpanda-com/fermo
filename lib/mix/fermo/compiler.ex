df427ccfd322cfb94d51d814b780dd0925f877f7
Joe Yates <joe.g.yates@gmail.com>
2020-01-06 17:39:03 +0100

    Extract Manifest module

--- lib/mix/fermo/compiler.ex
+++ lib/mix/fermo/compiler.ex
@@ -1,14 +1,13 @@
 defmodule Mix.Fermo.Compiler do
   import Mix.Fermo.Paths
-
-  @manifest_vsn 1
+  alias Mix.Fermo.Compiler.Manifest
 
   def run() do
     compilation_timestamp = compilation_timestamp()
     all_sources = all_sources()
-    changed = changed_since(all_sources, manifest_timestamp())
+    changed = changed_since(all_sources, Manifest.timestamp())
     IO.puts "changed: #{inspect(changed, [pretty: true, width: 0])}"
-    write_manifest(all_sources, compilation_timestamp)
+    Manifest.write(all_sources, compilation_timestamp)
   end
 
   defp changed_since(paths, timestamp) do
@@ -16,33 +15,9 @@ defmodule Mix.Fermo.Compiler do
   end
 
   defp all_sources do
-    Mix.Utils.extract_files([full_source_path], [:slim])
+    Mix.Utils.extract_files([full_source_path()], [:slim])
     |> MapSet.new()
   end
 
-  defp manifest do
-    if File.exists?(manifest_path()) do
-      case File.read!(manifest_path()) |> :erlang.binary_to_term() do
-        [@manifest_vsn | sources] -> sources
-        _ -> MapSet.new({})
-      end
-    else
-      MapSet.new([])
-    end
-  end
-
-  defp write_manifest(sources, timestamp) do
-    manifest_data =
-      [@manifest_vsn | sources]
-      |> :erlang.term_to_binary([:compressed])
-    File.write!(manifest_path(), manifest_data)
-    File.touch!(manifest_path(), timestamp)
-  end
-
   def compilation_timestamp, do: System.os_time(:second)
-  def manifest_timestamp, do: Mix.Utils.last_modified(manifest_path())
-
-  defp manifest_path do
-    Path.join(Mix.Project.manifest_path(), "compile.fermo")
-  end
 end
