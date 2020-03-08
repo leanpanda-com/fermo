af4fde71aaa4b3046aec362817b538c23d6bf76c
Joe Yates <joe.g.yates@gmail.com>
2020-03-08 18:25:36 +0100

    Release an Elixir 1.10 compatible version of 0.6

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Fermo.MixProject do
   def project do
     [
       app: :fermo,
-      version: "0.6.3",
+      version: "0.6.4",
       elixir: "~> 1.4",
       description: "A static site generator",
       package: package(),
@@ -31,7 +31,7 @@ defmodule Fermo.MixProject do
       {:ex_doc, "~> 0.19", only: :dev},
       {:fermo_helpers, "~> 0.6.0"},
       {:jason, "~> 1.1"},
-      {:morphix, "~> 0.0.7"},
+      {:morphix, "~> 0.8.0"},
       {:slime, "1.0.0"},
       {:yaml_elixir, "~> 1.3.0"}
     ]
