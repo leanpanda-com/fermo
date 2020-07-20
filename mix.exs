6658bbf5774e0eb68b6c3045aaae61ce4448b01b
Joe Yates <joe.g.yates@gmail.com>
2020-07-20 10:41:46 +0200

    Update dependency

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Fermo.MixProject do
   use Mix.Project
 
-  @version "0.8.4"
+  @version "0.9.0"
   @git_origin "https://github.com/leanpanda-com/fermo"
 
   def project do
@@ -35,7 +35,7 @@ defmodule Fermo.MixProject do
   defp deps do
     [
       {:ex_doc, "~> 0.19", only: :dev},
-      {:fermo_helpers, "~> 0.8.4"},
+      {:fermo_helpers, "~> 0.9.0"},
       {:slime, "1.0.0"},
       {:yaml_elixir, "~> 1.3.0"}
     ]
