e1980cae277d1887a6eff739e6669c58503cc4e8
Joe Yates <joe.g.yates@gmail.com>
2020-07-25 18:25:57 +0200

    Update dependency

--- mix.exs
+++ mix.exs
@@ -35,7 +35,7 @@ defmodule Fermo.MixProject do
   defp deps do
     [
       {:ex_doc, "~> 0.19", only: :dev},
-      {:fermo_helpers, "~> 0.9.0"},
+      {:fermo_helpers, "~> 0.9.1"},
       {:slime, "1.0.0"},
       {:yaml_elixir, "~> 1.3.0"}
     ]
