576ff3c027d4c67d3123b3096178205e2b10ccc0
Joe Yates <joe.g.yates@gmail.com>
2020-10-14 10:16:09 +0200

    Create a fermo.live mix task

---
+++ lib/mix/tasks/fermo.live.ex
@@ -0,0 +1,11 @@
+defmodule Mix.Tasks.Fermo.Live do
+  use Mix.Task
+
+  @shortdoc "Serves the built site"
+
+  @moduledoc """
+  Serves the files from the 'build' directory
+  """
+  def run(_args) do
+  end
+end
