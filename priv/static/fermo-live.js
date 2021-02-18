e84eac7e75a0e8cb13f67911c4c1fc4e5072fbac
Joe Yates <joe.g.yates@gmail.com>
2021-02-18 10:03:51 +0100

    Use a closure

--- priv/static/fermo-live.js
+++ priv/static/fermo-live.js
@@ -1,41 +1,44 @@
-const FERMO_LIVE = {
-  socketPath: 'ws://' + window.location.host + '/__fermo/ws/live-reload',
-  pingTimer: null
-}
-FERMO_LIVE.socket = new window.WebSocket(FERMO_LIVE.socketPath)
+const FERMO_LIVE_SOCKET = ((location) => {
+  const socketPath = `ws://${location.host}/__fermo/ws/live-reload`
+  const socket = new window.WebSocket(socketPath)
+  let pingTimer = null
 
-const reloadPage = () => {
-  window.location.reload()
-}
-
-FERMO_LIVE.socket.onopen = e => {
-  FERMO_LIVE.socket.send('subscribe:live-reload:' + window.location.pathname)
-  FERMO_LIVE.pingTimer = window.setInterval(() => {
-    FERMO_LIVE.socket.send(JSON.stringify({event: 'ping'}))
-  }, 10000)
-}
-
-FERMO_LIVE.socket.onmessage = (event) => {
-  console.log('onmessage: ', event)
-  if (event.data === 'reload') {
-    reloadPage()
+  const reloadPage = () => {
+    window.location.reload()
   }
-}
 
-FERMO_LIVE.socket.onclose = event => {
-  // Clean up ping timer
-  if(FERMO_LIVE.pingTimer) {
-    window.clearInterval(FERMO_LIVE.pingTimer)
-    FERMO_LIVE.pingTimer = null
+  socket.onopen = e => {
+    console.log('socket onopen')
+    socket.send('subscribe:live-reload:' + window.location.pathname)
+    pingTimer = window.setInterval(() => {
+      socket.send(JSON.stringify({event: 'ping'}))
+    }, 10000)
   }
-  if (event.wasClean) {
-    console.log('onclose clean event:', event)
-  } else {
-    // TODO: Poll to try to reconnect
-    console.log('onclose died event:', event)
-  }
-}
 
-FERMO_LIVE.socket.onerror = error => {
-  console.log('onerror error:', error)
-}
+  socket.onmessage = (event) => {
+    console.log('onmessage: ', event)
+    if (event.data === 'reload') {
+      reloadPage()
+    }
+  }
+
+  socket.onclose = event => {
+    // Clean up ping timer
+    if(pingTimer) {
+      window.clearInterval(pingTimer)
+      pingTimer = null
+    }
+    if (event.wasClean) {
+      console.log('onclose clean event:', event)
+    } else {
+      // TODO: Poll to try to reconnect
+      console.log('onclose died event:', event)
+    }
+  }
+
+  socket.onerror = error => {
+    console.log('onerror error:', error)
+  }
+
+  return socket
+})(window.location)
